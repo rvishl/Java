8b92ac923c8f24393cf1066863b66564a9a4c050
vishal Raj <rvishl05@gmail.com>
2022-10-13 11:16:22 +0530

    L5

---
+++ Geeks/L5 - 17-Apr/practice.java
@@ -0,0 +1,70 @@
+import java.util.Scanner;
+
+class practice {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Enter the number of rows that you want: ");
+        int n = sc.nextInt();
+
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=n; j>=i; j--) {
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print(" " + j + " ");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // for(int i=1; i<=n; i++) {
+        //     int p=1;
+        //     for(int j=n; j>=i; j--) {
+        //         // System.out.print(j);
+        //         System.out.print(" " + p + " ");
+        //         p++;
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print(" * ");
+        //     }
+        //     System.out.print("\n");
+        // }
+        // for(int i=1; i<n; i++) {            
+        //     for(int j=n; j>i; j--) {
+        //         System.out.print(" * ");
+        //     }
+        //     if(i == 5) {break;}
+        //     System.out.print("\n");
+        // }
+
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+        // for(int i=1; i<n; i++) {
+        //     for(int j=n-1; j>=i; j--) {
+        //         System.out.print("*");
+        //     }
+        //     if(i == 5)
+        //         break;
+        //     System.out.print("\n");
+        // }
+
+    }
+}
