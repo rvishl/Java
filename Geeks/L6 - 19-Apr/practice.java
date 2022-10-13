54552e5cee40fbf36b85d96791bca7fc1da65e15
vishal Raj <rvishl05@gmail.com>
2022-10-13 17:09:23 +0530

    pattern

--- Geeks/L6 - 19-Apr/practice.java
+++ Geeks/L6 - 19-Apr/practice.java
@@ -9,11 +9,41 @@ class practice {
         //     System.out.println();
         // }
 
-        for(int i=1; i<=4; i++) {
-            for(int j=1; j<=4; j++) {
-                System.out.print((j + (i-1) * 4) + " ");
+        // for(int i=1; i<=4; i++) {
+        //     for(int j=1; j<=4; j++) {
+        //         System.out.print((j + (i-1) * 4) + " ");
+        //     }
+        //     System.out.println();
+        // }
+
+        int n=6;
+        // for(int i=1; i<=n; i++) {
+        //     for(int j=1; j<=i; j++) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+        // for(int i=1; i<n; i++) {
+        //     for(int j=n-1; j>=i; j--) {
+        //         System.out.print("*");
+        //     }
+        //     System.out.print("\n");
+        // }
+
+        for(int i=1; i<=n; i++) {
+            for(int j=1; j<=i; j++) {
+                System.out.print(j);
             }
-            System.out.println();
+            System.out.print("\n");
         }
+        for(int i=1; i<n; i++) {
+            int p=1;
+            for(int j=n-1; j>=i; j--) {
+                System.out.print(p);
+                p++;
+            }
+            System.out.print("\n");
+        }
+
     }
 }
