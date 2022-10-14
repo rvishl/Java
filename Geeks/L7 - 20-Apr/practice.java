f8f4eb3b9a503e8a0a9f579e421e17bdfec159b7
vishal Raj <rvishl05@gmail.com>
2022-10-14 16:50:39 +0530

    function

---
+++ Geeks/L7 - 20-Apr/practice.java
@@ -0,0 +1,61 @@
+import java.util.Scanner;
+// class practice {
+//     public static Scanner sc = new Scanner(System.in);
+
+//     // public static int sum(int a, int b) {
+//     //     return (a+b);
+//     // }
+
+//     // public static int add(int a, int b) {
+//     //     return (a+b);
+//     // }
+
+//     // public static int sum(int a, int b, int c) {
+//     //     return (a+b+c);
+//     // }
+
+//     public static void main(String[] args) {
+//         // int c = sum(10, 15);
+//         // System.out.print(c);
+
+//         long l = (long)2147483648;
+//         System.out.print(l);
+//     }
+// }
+
+// class practice {
+//     public static Scanner sc = new Scanner(System.in);
+
+//     static void add(int a, int b) {
+//         System.out.print(a+b);
+//     }
+
+//     static void sub(int a, int b) {
+//         System.out.print(a-b);
+//     }
+
+//     static void mul(int a, int b) {
+//         System.out.print(a*b);
+//     }
+
+//     public static void main(String[] args) {
+//         System.out.print("Enter first no: ");
+//         int a = sc.nextInt();
+//         System.out.print("Enter second no: ");
+//         int b = sc.nextInt();
+//         System.out.print("Enter operator: ");
+//         char ch = sc.next().charAt(0);
+
+//         if(ch == '+') {
+//             add(a, b);
+//         } else if(ch == '-') {
+//             sub(a, b);
+//         } else if(ch == '*') {
+//             mul(a, b);}
+//         // } else if(ch == '/') {
+//         //     div(a, b);
+//         // } else if(ch == '%') {
+//         //     rem(a, b);
+//         // }
+//     }
+// }
