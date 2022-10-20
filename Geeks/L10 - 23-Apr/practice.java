df3e64eb15be2a4f61ad3754d4392d14caa44f28
vishal Raj <rvishl05@gmail.com>
2022-10-20 10:41:32 +0530

    L10

---
+++ Geeks/L10 - 23-Apr/practice.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class practice {
+    public static Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) {
+        int[] arr = new int[5];
+        for(int i=0; i<5; i++) {
+            System.out.print(arr[i] + " ");
+        }
+    }
+}
